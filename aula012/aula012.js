a5bfa0a508846709c8c09996b2d43538d128a01b
Fabio Silva <fabio.costa@al.infnet.edu.br>
2021-07-09 02:34:54 -0300

    commit

--- aula012/aula012.js
+++ aula012/aula012.js
@@ -1,3 +1,11 @@
 let umaString = 'Um "texto"';
 
-console.log(umaString)
+console.log(umaString.concat(' em um ', 'lindo dia.'))
+console.log(`${umaString} em um lindo dia`)
+
+console.log(umaString.replace(/Um/, 'outra'));
+
+//Verifica quantos caracteres tem
+console.log(umaString.length);
+
+console.log(umaString.slice(0, 5));
